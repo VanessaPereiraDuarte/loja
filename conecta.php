24b8d8904e9cbe6f320285d8a4d0c4a2033a0198
Eduardo - Notebook <eduardo-tell@hotmail.com>
2019-02-09 17:51:58 -0200

    Atualiando conexão com banco de dados

--- conecta.php
+++ conecta.php
@@ -1,2 +1,9 @@
 <?php
-$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+$conexao = mysqli_connect('sql10.freemysqlhosting.net:3306', 'sql10278205', 'uQ3lWS3Wf3', 'sql10278205');
+
+if (!$conexao) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
