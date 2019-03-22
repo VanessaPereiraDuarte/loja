0579c7e9b3e10bd2b12d863e2a6297ddcbfcf8c2
Vanessa <vanessaduarte.sc@hotmail.com>
2019-03-21 22:02:36 -0300

    alterações

--- lateral.php
+++ lateral.php
@@ -5,6 +5,13 @@
             <li>
                 <a href="#"><i class="fas fa-home home"></i> Minha loja <i class="fas fa-angle-right flexa"></i></a>
             </li>
+            <li>
+                <a href="#"></i> Adiciona Produtos</i></a>
+            </li>
+            <li>
+                <a href="#"></i>Produtos </i></a>
+            </li>
+            
         </ul>
     </nav>
 </div>
